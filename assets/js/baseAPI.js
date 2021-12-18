b11cbbbade9b17922cc509cb3021bdb2d4b08095
項哲 <593785042@qq.com>
2021-12-18 16:35:27 +0800

    完成了主页功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,20 @@
 $.ajaxPrefilter(function(options){
     options.url = 'http://www.liulongbin.top:3007' + options.url
-    console.log(options.url);
+   
+    // console.log(options.url.indexOf('/my/'));
+    // console.log(options.url);
+    if(options.url.indexOf('/my/') !== -1){
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+         }
+    }
+
+    // 统一挂载 complete
+
+    options.complete = function(res){
+        if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！' ){
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
